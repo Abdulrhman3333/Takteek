93713d31527e372dd00d99a363bb38a7bfbf199b
XD7myyX <89908274+XD7myyX@users.noreply.github.com>
2022-06-28 22:42:39 +0300

    paging records page

--- admin/records.php
+++ admin/records.php
@@ -3,6 +3,16 @@ $title = "السجل" ;
 require_once "header.php";
 
 $records = $conn->query("SELECT * FROM records");
+if (!isset ($_GET['page']) ) {  
+    $page = 1;  
+} else {  
+    $page = $_GET['page'];  
+}  
+$results_per_page = 10;  
+$page_first_result = ($page-1) * $results_per_page;
+//determine the total number of pages available  
+$number_of_page = ceil (mysqli_num_rows($records) / $results_per_page);
+$records = $conn->query("SELECT * FROM records LIMIT " . $page_first_result . ',' . $results_per_page);
 ?>
 
 <table>
@@ -17,3 +27,11 @@ $records = $conn->query("SELECT * FROM records");
     </table>
 
     <!-- make pages -->
+<ul>
+    <?php 
+    for ($i=1; $i <= $number_of_page ; $i++){
+        echo "<li><a href='records.php?page=$i'>".$i."</li>";
+    }
+    ?>
+        
+</ul>
